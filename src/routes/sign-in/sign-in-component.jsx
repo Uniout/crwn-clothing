2287e488fa30d3111691d388dc9f941df15f337a
Uniout <stampontheground@gmail.com>
2023-03-06 17:06:01 +0100

    add sign in and sing up components with styling

--- src/routes/sign-in/sign-in-component.jsx
+++ src/routes/sign-in/sign-in-component.jsx
@@ -1,9 +1,27 @@
+import { useEffect } from 'react';
+import { getRedirectResult } from 'firebase/auth';
+
+import SignUpForm from '../../components/sign-up-form/sign-up-form.component';
+
+
 import { 
+    auth,
     signInWithGooglePopup,
-    createUserDocumentFromAuth
+    createUserDocumentFromAuth,
+    signInWithGoogleRedirect
 } from '../../utils/firebase/firebase.utils';
 
 const SignIn = () => {
+    useEffect(() => {
+        const fetchData = async () => {
+            const response = await getRedirectResult(auth);
+            if(response) {
+                const userDocRef = await createUserDocumentFromAuth(response.user);
+            }
+        }
+        fetchData();
+    }, [])
+
     const logGoogleUser = async () => {
         const {user} = await signInWithGooglePopup();
         const userDocRef = await createUserDocumentFromAuth(user);
@@ -15,6 +33,10 @@ const SignIn = () => {
             <h1>Sign In Page</h1>
             <button onClick={logGoogleUser}>
                 Sign In with Google Popup
+            </button>
+            <SignUpForm />
+            <button onClick={signInWithGoogleRedirect}>
+                Sign In with Google Redirect
             </button>
         </div>
     )
