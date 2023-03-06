aebb68c77a45204d31f03984ad7f93d6188ff9b9
Uniout <stampontheground@gmail.com>
2023-03-06 17:11:54 +0100

    add sing up form styling

--- src/components/sign-up-form/sign-up-form.component.jsx
+++ src/components/sign-up-form/sign-up-form.component.jsx
@@ -2,8 +2,9 @@ import { useState } from 'react';
 
 import FormInput from '../form-input/form-input.component';
 
-
 import { createAuthUserWithEmailAndPassword, createUserDocumentFromAuth } from '../../utils/firebase/firebase.utils';
+
+import './sign-up-form.style.scss';
 
 const defaultFormFields = {
   displayName: '',
@@ -52,8 +53,9 @@ const SignUpForm = () => {
   }
 
   return (
-    <div>
-      <h1>Sign up with your email and password</h1>
+    <div className='sign-up-container'>
+      <h2>Don't have an account?</h2>
+      <span>Sign up with your email and password</span>
       <form onSubmit={handleSubmit}>
         <FormInput 
           label="Display Name"
