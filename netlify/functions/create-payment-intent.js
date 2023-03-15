407d539667fbce17ff1fe58640f454e85ebbaaf5
Uniout <stampontheground@gmail.com>
2023-03-15 11:03:25 +0100

    add stripe integration for payment

---
+++ netlify/functions/create-payment-intent.js
@@ -0,0 +1,31 @@
+require("dotenv").config();
+const stripe = require("stripe")(process.env.REACT_APP_STRIPE_SECRET_KEY);
+
+exports.handler = async (event) => {
+  try {
+    const { amount } = JSON.parse(event.body);
+
+    const paymentIntent = await stripe.paymentIntents.create({
+      amount,
+      currency: "pln",
+      payment_method_types: ["card"],
+    });
+
+    return {
+      statusCode: 200,
+      headers: {
+        "Access-Control-Allow-Headers": "Content-Type",
+        "Access-Control-Allow-Origin": "*",
+        "Access-Control-Allow-Methods": "OPTIONS,POST,GET",
+      },
+      body: JSON.stringify({ paymentIntent }),
+    };
+  } catch (error) {
+    console.log({ error });
+
+    return {
+      statusCode: 400,
+      body: JSON.stringify({ error }),
+    };
+  }
+};
